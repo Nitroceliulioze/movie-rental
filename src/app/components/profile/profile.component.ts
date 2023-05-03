356fc779d9b73d61c18dcf405ef6f3bc49d79e2c
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-03 11:06:58 +0300

    Profile page renders user info from sessionStorage

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  name!: string;
+  surname!: string;
+  email!:string;
 
+  constructor(private service: UserService) {}
+
+  ngOnInit(): void {
+    const email: any = sessionStorage.getItem('email');
+    console.log(email);
+    this.service.getUserByEmail(email).subscribe({
+      next: (users: any) => {
+        const user = users[0];
+        console.log(user);
+        this.name = user.firstName;
+        this.surname = user.surname;
+        this.email = user.email;
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
