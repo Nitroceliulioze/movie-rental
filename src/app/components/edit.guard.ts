906c24a9761b6fc75a73cf62687c0189fa869802
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-04 09:29:37 +0300

    fix editGuard

--- src/app/components/edit.guard.ts
+++ src/app/components/edit.guard.ts
@@ -1,21 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanDeactivate } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EditGuard implements CanDeactivate<ProfileEditComponent> {
   canDeactivate(
-    component: ProfileEditComponent,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if(component.editForm.dirty) {
-        return confirm('All unsaved changes will be lost')
-      }
+    component: ProfileEditComponent
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (component.editForm.dirty) {
+      return confirm('All unsaved changes will be lost');
+    }
     return true;
   }
-  
 }
